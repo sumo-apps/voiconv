0ecf5dcf5465713a3842bf4b738f2de0efb907a3
tuomo <tuomo.hirvonen@gmail.com>
2023-03-27 23:40:41 +0300

    cleaned up a little bit

--- engine_src/compress.cpp
+++ engine_src/compress.cpp
@@ -35,21 +35,18 @@ using namespace TEN;
 MemBuffer *TEN::lzma_compress(MemBuffer *from, bool deleteFrom)
 {
     printf("lzma_compress: original %ld bytes. %f.2 MB\n", from->size(), (float)from->size() / (1024 * 1024));
-    // uint32_t compressed_size = 0;
-    // unsigned char *compressed_data = lzmaCompress( from->data, (uint32_t)from->size(), &compressed_size)
     //  set up properties
     SizeT inputSize = from->size();
     CLzmaEncProps props;
     LzmaEncProps_Init(&props);
 
     props.dictSize = inputSize;
+    
     /*
+		note, NOT heavily tested which attributes work the best in which case. These values are just good guesses
     if (props.dictSize > (1 << 20))
         props.dictSize = (1 << 20);
         */
-    if (props.dictSize > (1 << 19))
-        props.dictSize = (1 << 19);
-
     if (props.dictSize < (1 << 12))
         props.dictSize = (1 << 12);
 
@@ -68,7 +65,6 @@ MemBuffer *TEN::lzma_compress(MemBuffer *from, bool deleteFrom)
     SizeT outputSize64 = inputSize * 1.25;
     if (outputSize64 < 1024)
         outputSize64 = 1024;
-    // auto output = std::make_unique<uint8_t[]>(outputSize64);
     Byte *outputData = (Byte *)malloc(outputSize64);
 
     int lzmaStatus = LzmaEncode(
@@ -79,7 +75,6 @@ MemBuffer *TEN::lzma_compress(MemBuffer *from, bool deleteFrom)
 
     printf("LZMA compression finished: %d bytes (%.2f Mb)\n", (int)outputSize64, (float)outputSize64 / (1024 * 1024));
     // destroy the inputbuffer
-
     if (deleteFrom)
         delete from;
 
@@ -100,26 +95,12 @@ MemBuffer *TEN::lzma_compress(MemBuffer *from, bool deleteFrom)
         memcpy(result + 13 + 8, outputData, outputSize64);
         free(outputData);
         return new MemBuffer(result, outputSize, true);
-        /*
-        size_t outputSize = outputSize64 + 13;
-        // tricky: we have to generate the LZMA header
-        // 5 bytes properties + 8 byte uncompressed size
-        // result = std::make_unique<uint8_t[]>(outputSize64 + 13);
-        Byte *result = (Byte *)malloc(outputSize);
-        memcpy(result, propsEncoded, 5);
-        for (int i = 0; i < 8; i++)
-            result[5 + i] = (inputSize >> (i * 8)) & 0xFF;
-        memcpy(result + 13, outputData, outputSize64);
-        free(outputData);
-        return new MemBuffer(result, outputSize, true);
-        */
     }
     else
     {
         free(outputData);
         printf("lzma_compress did not succeed.\n");
     }
-
     return 0;
 }
 
@@ -144,12 +125,6 @@ MemBuffer *TEN::lzma_decompress(MemBuffer *from, bool deleteFrom)
 
     // extract the size from the header
     UInt64 size = 0;
-    /*
-    for (int i = 0; i < 8; i++) {
-        printf("SizeChar %d: %d (hex: %x) \n", i, input[5+i], input[5+i]);
-        size |= (((UInt64)input[5 + i]) << (i * 8));
-    }
-    */
     memcpy(&size, input + 5, 8);
 
     printf("Original size is: %zu (%.2f). Target size is %llu bytes (%.2f Mb)\n", inputSize, (float)inputSize/(1024*1024), size, (float)size / (1024 * 1024));
@@ -176,35 +151,4 @@ MemBuffer *TEN::lzma_decompress(MemBuffer *from, bool deleteFrom)
     if (deleteFrom)
         delete from;
     return NULL;
-
-    /*
-    if (from->size() < 13)
-        return NULL; // invalid header!
-
-    Byte *input = (Byte *)from->data;
-    SizeT inputSize = (SizeT)from->size();
-    // extract the size from the header
-    UInt64 size = 0;
-    for (int i = 0; i < 8; i++)
-        size |= (input[5 + i] << (i * 8));
-
-    if (size <= (256 * 1024 * 1024))
-    {
-        // auto blob = std::make_unique<uint8_t[]>(size);
-        uint8_t *blob = (uint8_t *)malloc(size);
-
-        ELzmaStatus lzmaStatus;
-        SizeT procOutSize = size;
-        SizeT procInSize = inputSize - 13;
-        int status = LzmaDecode(blob, &procOutSize, &input[13], &procInSize, input, 5, LZMA_FINISH_END, &lzmaStatus, &_allocFuncs);
-        delete from;
-
-        if (status == SZ_OK && procOutSize == size)
-        {
-            return new MemBuffer(blob, size, true);
-        }
-    }
-    if (deleteFrom) delete from;
-    return NULL;
-    */
 }
